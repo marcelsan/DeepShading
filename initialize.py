7f1c5799dab218aaf473f3f97fcff8c26066a923
Marcel Santos <mss8@cin.ufpe.br>
2018-11-13 23:22:57 -0300

    Adjusts on project structure.

--- initialize.py
+++ initialize.py
@@ -5,3 +5,9 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer ('imSize', 512, 'Input image size.')
 
 tf.app.flags.DEFINE_float 	('learningRate', 0.00009, 'Learning rate for ADAM.')
+
+tf.app.flags.DEFINE_string  ('trainSetDir', '/media/marcelsantos/DATA/DeepShadingDataBase/train_512.tfrecord',
+							'Directory for the training dataset.')
+
+tf.app.flags.DEFINE_string  ('valSetDir', '/media/marcelsantos/DATA/DeepShadingDataBase/validation_512.tfrecord',
+							'Directory for the validation dataset.')
--- shading.py
+++ shading.py
@@ -5,65 +5,62 @@ import numpy as np
 import tensorflow as tf
 
 from initialize import FLAGS
-from losses import perceptual_loss, l1_loss
+from losses import perceptual_loss, l1_loss, l2_loss
 from models.shading_net import ShadingNet
+from models.res_shading_net import ResShadingNet
 from tensorflow import keras
 
-train_set = '/media/marcelsantos/DATA/DeepShadingDataBase/train_512.tfrecord'
-val_set = '/media/marcelsantos/DATA/DeepShadingDataBase/validation_512.tfrecord'
-
-model = ShadingNet()
-model.summary()
-
-#=== Loss function formulation =================================================
 lr = FLAGS.learningRate
-loss_function = 'perceptual_loss'
+loss_function = 'l2_loss'
 
-model.compile(optimizer=tf.train.AdamOptimizer(lr), loss=l1_loss)
+#=== Load and compile model ====================================================
+model = ResShadingNet()
+model.summary()
+# model.compile(optimizer=tf.train.AdamOptimizer(lr), loss=l2_loss)
 
-#=== Setup Data ================================================
-featdef = { 'input': tf.FixedLenFeature(shape=[], dtype=tf.string),
-           'ground_truth': tf.FixedLenFeature(shape=[], dtype=tf.string)}
+# #=== Setup Data =================================================================
+# featdef = { 'input': tf.FixedLenFeature(shape=[], dtype=tf.string),
+#            'ground_truth': tf.FixedLenFeature(shape=[], dtype=tf.string)}
           
-def _parse_record(example_proto, clip=False):
-    """Parse a single record into image, weather labels, ground labels"""
-    example = tf.parse_single_example(example_proto, featdef)
-    im = tf.decode_raw(example['input'], tf.float32)
-    im = tf.reshape(im, (FLAGS.imSize, FLAGS.imSize, 6))
-    gt = tf.decode_raw(example['ground_truth'], tf.float32)
-    gt = tf.reshape(gt, (FLAGS.imSize, FLAGS.imSize, 1))
+# def _parse_record(example_proto, clip=False):
+#     """Parse a single record into image, weather labels, ground labels"""
+#     example = tf.parse_single_example(example_proto, featdef)
+#     im = tf.decode_raw(example['input'], tf.float32)
+#     im = tf.reshape(im, (FLAGS.imSize, FLAGS.imSize, 6))
+#     gt = tf.decode_raw(example['ground_truth'], tf.float32)
+#     gt = tf.reshape(gt, (FLAGS.imSize, FLAGS.imSize, 1))
 
-    return im, gt
+#     return im, gt
 
-# Construct a TFRecordDataset.
-ds_train = tf.data.TFRecordDataset(train_set).map(_parse_record)
-ds_train = ds_train.shuffle(1000).batch(16).repeat()
+# # Construct a TFRecordDataset.
+# ds_train = tf.data.TFRecordDataset(FLAGS.trainSetDir).map(_parse_record)
+# ds_train = ds_train.shuffle(1000).batch(16).repeat()
 
-ds_validation = tf.data.TFRecordDataset(val_set).map(_parse_record)
-ds_validation = ds_validation.batch(8).repeat()
+# ds_validation = tf.data.TFRecordDataset(FLAGS.valSetDir).map(_parse_record)
+# ds_validation = ds_validation.batch(8).repeat()
 
-#=== Run training loop ============================================================
+# #=== Run training loop ============================================================
 
-# Define the callbacks.
-time_now = datetime.datetime.now()
-output_file_name = 'shading__lr_%s__loss_%s__%s' % (lr, loss_function, time_now)
+# # Define the callbacks.
+# time_now = datetime.datetime.now()
+# output_file_name = 'deep_shading__lr_%s__loss_%s__%s' % (lr, loss_function, time_now)
 
-callbacks = [
-  # Write log on CSV.
-  tf.keras.callbacks.CSVLogger('./results/logs/%s_training.log' % (output_file_name))
-]
+# callbacks = [
+#   # Write log on CSV.
+#   tf.keras.callbacks.CSVLogger('./results/logs/%s_training.log' % (output_file_name))
+# ]
 
-# Fit the model.
-history = model.fit(ds_train, epochs=40, steps_per_epoch=500, 
-                    validation_data=ds_validation, validation_steps=15, 
-                    callbacks=callbacks)
+# # Fit the model.
+# history = model.fit(ds_train, epochs=10, steps_per_epoch=500, 
+#                     validation_data=ds_validation, validation_steps=15, 
+#                     callbacks=callbacks)
 
-#=== Shut down ===================================================================
+# #=== Shut down ===================================================================
 
-# Save the model.
-model_json = model.to_json()
-with open("./results/models/%s_model.json" % (output_file_name) , "w") as json_file:
-    json_file.write(model_json)
+# # Save the model.
+# model_json = model.to_json()
+# with open("./results/models/%s_model.json" % (output_file_name) , "w") as json_file:
+#     json_file.write(model_json)
 
-model.save('./results/models/%s_model.h5' % (output_file_name))
-print('[INFO] Model saved.')
+# model.save('./results/models/%s_model.h5' % (output_file_name))
+# print('[INFO] Model saved.')
